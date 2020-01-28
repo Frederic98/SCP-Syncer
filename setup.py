da4b8da3667e852b97fd46ac1360b7776dc0dd5a
Frederic Meeuwissen <frederic.meeuwissen@gmail.com>
2020-01-28 15:37:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name="SCPSycer",
+    name="SCPSyncer",
     version="0.1",
     packages=find_packages(),
     install_requires=['paramiko', 'scp', 'PyYAML', 'colorama'],
 
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     # metadata to display on PyPI
     author="Frederic98",
     description="SCP syncer",
