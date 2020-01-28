33d4af1a50295991b17af439347964f6c28a9f84
Frederic Meeuwissen <frederic.meeuwissen@gmail.com>
2020-01-28 11:53:16 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+setup(
+    name="SCPSycer",
+    version="0.1",
+    packages=find_packages(),
+
+    # metadata to display on PyPI
+    author="Frederic98",
+    description="SCP syncer",
+    keywords="scp sync syncer",
+    url="https://github.com/Frederic98/SCP-Syncer"
+)
